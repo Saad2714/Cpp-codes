34efaa32556bfc63df2790f63080c2e684464c02
Satwik-ERROR404 <Satwik.Samayamantry@iiitb.org>
2019-10-20 22:53:26 +0530

    modified if-else statement using another condition

--- string.cpp
+++ string.cpp
@@ -8,16 +8,23 @@ int main()
     string s, s1;
     s = "HELLO";
     s1 = "HELLO";
+    int check1=0,check2=0;
     if(s.compare(s1) == 0)
+    {
+	    check1=1;
         cout << s << " is equal to " << s1 << endl;
-    else
+    }
+    if(check1==0)
         cout << s << " is not equal to " << s1 << endl;
     s.append(" WORLD!");
     cout << s << endl;
     printf("%s\n", s.c_str());
     if(s.compare(s1) == 0)
+    {
+	    check2=1;
         cout << s << " is equal to " << s1 << endl;
-    else
+    }
+    if(check2==0)
         cout << s << " is not equal to " << s1 << endl;
     return 0;
 }
